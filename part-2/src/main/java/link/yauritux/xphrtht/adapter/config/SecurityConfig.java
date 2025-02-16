904e8fc47fb3320cb0df2ac91d955bb07ff7c53b
Yauri Attamimi <yauritux@gmail.com>
2025-02-16 22:09:25 +0700

    feat: add security config, implement in-memory user data

---
+++ part-2/src/main/java/link/yauritux/xphrtht/adapter/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package link.yauritux.xphrtht.adapter.config;
+
+import link.yauritux.xphrtht.core.domain.vo.UserRole;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author Yauri Attamimi
+ * @version 0.1
+ */
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth ->
+                auth.requestMatchers("/web/reports/work_hours")
+                        .hasAnyRole(UserRole.EMPLOYEE.name(), UserRole.ADMIN.name()).anyRequest().permitAll()
+        ).formLogin(form -> form.loginPage("/login").permitAll()).logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails tom = User.builder()
+                .username("tom").password(passwordEncoder.encode("tom123"))
+                .roles(UserRole.EMPLOYEE.name()).build();
+        UserDetails jerry = User.builder()
+                .username("jerry").password(passwordEncoder.encode("jerry123"))
+                .roles(UserRole.EMPLOYEE.name()).build();
+        UserDetails admin = User.builder()
+                .username("admin").password(passwordEncoder.encode("admin123"))
+                .roles(UserRole.ADMIN.name()).build();
+        return new InMemoryUserDetailsManager(tom, jerry, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ part-2/src/main/java/link/yauritux/xphrtht/core/domain/vo/UserRole.java
@@ -0,0 +1,10 @@
+package link.yauritux.xphrtht.core.domain.vo;
+
+/**
+ * @author Yauri Attamimi
+ * @version 0.1
+ */
+public enum UserRole {
+
+    EMPLOYEE, ADMIN;
+}
