b40cfeddcae8555729c9f7212643a553e5ffbe68
Yauri Attamimi <yauritux@gmail.com>
2025-02-16 14:49:36 +0700

    refactor: rename sql patch script to be specific according its purpose

---
+++ part-1/db/patches/time_tracking_btreeidx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "DROP INDEX IF EXISTS idx_time_records_time_from;" | docker container exec -i xphr-db psql -h localhost -U postgres -d xphr
+echo "CREATE INDEX idx_time_records_time_from ON time_records(time_from);" | docker container exec -i xphr-db psql -h localhost -U postgres -d xphr
+echo "DROP INDEX IF EXISTS idx_time_records_employee_id;" | docker container exec -i xphr-db psql -h localhost -U postgres -d xphr
+echo "CREATE INDEX idx_time_records_employee_id ON time_records(employee_id);" | docker container exec -i xphr-db psql -h localhost -U postgres -d xphr
+echo "DROP INDEX IF EXISTS idx_time_records_project_id;" | docker container exec -i xphr-db psql -h localhost -U postgres -d xphr
+echo "CREATE INDEX idx_time_records_project_id ON time_records(project_id);" | docker container exec -i xphr-db psql -h localhost -U postgres -d xphr
