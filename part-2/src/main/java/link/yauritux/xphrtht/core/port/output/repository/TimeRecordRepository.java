77ea029ac6f4e4362426c75bec67d0addf980da4
Yauri Attamimi <yauritux@gmail.com>
2025-02-16 16:14:05 +0700

    feat: implement time tracking report query

--- part-2/src/main/java/link/yauritux/xphrtht/core/port/output/repository/TimeRecordRepository.java
+++ part-2/src/main/java/link/yauritux/xphrtht/core/port/output/repository/TimeRecordRepository.java
@@ -1,11 +1,28 @@
 package link.yauritux.xphrtht.core.port.output.repository;
 
+import link.yauritux.xphrtht.core.domain.dto.EmployeeTimeTrackingReportDto;
 import link.yauritux.xphrtht.core.domain.entity.TimeRecord;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author Yauri Attamimi
  * @version 0.1
  */
-public interface TimeRecordRepository extends PagingAndSortingRepository<TimeRecord, Long> {
+public interface TimeRecordRepository extends JpaRepository<TimeRecord, Long> {
+
+    @Query("SELECT e.name AS employeeName, p.name AS projectName," +
+            "  SUM(EXTRACT(EPOCH FROM (tr.timeTo - tr.timeFrom)) / 3600) AS totalHours " +
+            "FROM TimeRecord tr " +
+            "JOIN Employee e ON tr.employee.id = e.id " +
+            "JOIN Project p ON tr.project.id = p.id " +
+            "WHERE tr.timeFrom BETWEEN :startDate AND :endDate " +
+            "GROUP BY e.employeeName, p.projectName " +
+            "ORDER BY e.employeeName, p.projectName")
+    List<EmployeeTimeTrackingReportDto> getTimeTrackingReport(
+            @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
